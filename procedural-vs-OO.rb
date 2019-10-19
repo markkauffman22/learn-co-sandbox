057cf57a27148b61ba42e3e8f67284b84195a68f
Mark Kauffman <markkauffman22@gmail.com>
2019-10-19 04:36:20 +0000

    Automatically backed up by Learn

--- procedural-vs-OO.rb
+++ procedural-vs-OO.rb
@@ -14,4 +14,33 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+<<<<<<< HEAD
 end
+=======
+end
+
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+ 
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+ 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+end
+
+
+
+>>>>>>> 35e1ca40e088a80ffa11ce30a848c557c2348a80
